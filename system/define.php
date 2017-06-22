b17088ba51a068dd02a8986318eb574059364ab9
storm1er <le.storm1er@gmail.com>
2017-06-22 20:43:25 +0200

    creation bdd en local ... tant pis

--- system/define.php
+++ system/define.php
@@ -17,11 +17,10 @@ if ($domaine == "dev-eco.lou-p.fr") {
 	@ini_set("error_reporting", "-1"); // désactivation des messages d'erreur en prod
 	@ini_set("display_errors", "-1"); // désactivation des messages d'erreur en prod
 	define("_DOMAIN_", "eco.lou-p.fr");
-	define("_HOST_", "188.165.53.185");
-	//define("_HOST_", "http://loupfrbddteco.mysql.db");
-	define("_USER_", "loupfrbddteco");
+	define("_HOST_", "localhost");
+	define("_USER_", "lou-p");
 	define("_PWD_", "1000cetN");
-	define ("_DB_", "loupfrbddteco");
+	define ("_DB_", "eco.lou-p.fr");
 } else {
 	die('Error : unknown domain');
 }
@@ -30,5 +29,3 @@ define("_ROOT_", dirname(__FILE__));         //system
 define("_CONT_", dirname(__FILE__).'/cont'); //system/cont
 define("_VIEW_", dirname(__FILE__).'/view'); //system/view
 define("_LIB_", dirname(__FILE__).'/lib');   //system/lib
-
-
