488f337798792d1664945dcf8ea3d618182e29a0
Louise <louise.ichigo@gmail.com>
2017-06-25 22:50:53 +0200

    2 3 modif

--- system/cont/main.php
+++ system/cont/main.php
@@ -2,24 +2,26 @@
 
 require_once _VIEW_.'/header.html';
 ?>
-<div class="log">
-			
-			<!-- condition si on est pas co ça affiche se connecter -->
-			<?php 
-			if(!isset($_SESSION['id'])){ ?>
-			<div class="conect">
-	                  <a class="grey-text text-lighten-3" href="login">Se connecter</a>      
-	        </div>
-	        <?php }  /*si on est co ça affiche se deconnecter*/
-	          else{ ?>
-	        <div class="disconect">
-	                  <a class="grey-text text-lighten-3" href="disconnect_post">Se deconnecter</a>
-	                
-	        </div>
-	        <?php } ?>
-			<!-- Fin de la condition -->
+	<div class="log">
 
-        </div>
+		<!-- condition si on est pas co ça affiche se connecter -->
+		<?php
+		if(!isset($_SESSION['id'])){ ?>
+			<div class="conect">
+			    <a class="grey-text text-lighten-3" href="login"><i class="fa fa-user-circle-o fa-2x" aria-hidden="true"></i></a>
+			</div>
+		<?php }  /*si on est co ça affiche se deconnecter*/
+		else{ ?>
+			<div class="disconect">
+			    <a class="grey-text text-lighten-3" href="disconnect_post"><i class="fa fa-user-times fa_2x" aria-hidden="true"></i></a>
+
+			</div>
+		<?php } ?>
+		<!-- Fin de la condition -->
+
+    </div>
+
+
 <?php
 require_once _VIEW_.'/'.$controller['folder'].'/'.$controller['file'].'.html';
 require_once _VIEW_.'/footer.html';
