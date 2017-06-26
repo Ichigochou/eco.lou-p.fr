a31fcc3cf57b5384a36d506c446d518aa3180425
Louise <louise.ichigo@gmail.com>
2017-06-26 10:05:11 +0200

    css

--- system/cont/login.php
+++ system/cont/login.php
@@ -1,18 +1,5 @@
-<div class="container" id="connect">
-	<div class="row">
-		<form class="col m12" method='post' action="login_post">
-			<div class="input-fiel col m6">
-			<input type="text" class="validate" name="login" placeholder="login">
-			<!-- <label for="login">login</label> -->
-			</div>
-			<div class="input-fiel col m6">
-			<input type="password" class="validate" name="password" placeholder="password">
-			<!-- <label for="password">password</label> -->
-			</div>
-			<div class="input-fiel col m12">
-			<input type="submit" class="waves-effect waves-purple lighten-3 btn">
+<?php require_once _VIEW_.'/header.html';
 
-			</div>
-		</form>
-	</div>
-</div>
+include _VIEW_."/login.html";
+
+require_once _VIEW_.'/footer.html';
